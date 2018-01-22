d10bbb80a6947d70ee5a21eaf5fc58501bfa109a
yyt_saber <yyt20080808@gmail.com>
2018-01-22 21:42:20 +0800

    there is  no changes

--- src/LinkedList/IntersectionofTwoLinkedLists.java
+++ src/LinkedList/IntersectionofTwoLinkedLists.java
@@ -5,7 +5,42 @@ package LinkedList;
  */
 public class IntersectionofTwoLinkedLists {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int N = 0;
+        int M = 0;
+        ListNode p = headA;
+        while (p != null) {
+            N++;
+            p = p.next;
+        }
+        p = headB;
+        while (p != null) {
+            M++;
+            p = p.next;
+        }
+        if (M == 0 || N ==0)
+            return null;
+        // 如果N 比M 大
+        p = headA;
+        while (N > M) {
+            p = p.next;
+            N--;
+        }
+        headA = p;
+        p = headB;
+        // 如果M 比N 大
+        while (M > N) {
+            p = p.next;
+            M--;
+        }
+        headB = p;
 
+        while(headA != null){
+            if(headA == headB)
+                return headA;
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return null;
     }
 
     public static void main(String[] args) {
